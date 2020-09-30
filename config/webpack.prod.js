da3d4fb9a33d2fd277b2d690761e8e62287a5c07
blacktail <mr.zhangping@gmail.com>
2020-09-30 20:13:44 +0800

    Fix a build error

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,6 +1,6 @@
 /* eslint-disable import/no-extraneous-dependencies */
 const path = require('path')
-const merge = require('webpack-merge')
+const { merge } = require('webpack-merge')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const TerserJSPlugin = require('terser-webpack-plugin')
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
